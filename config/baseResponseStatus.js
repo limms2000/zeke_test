c5c20234f30cccd550059264bf391dc57621b02a
limms2000 <limms1217@naver.com>
2021-03-25 16:02:21 +0900

    21.03.25 변경

--- config/baseResponseStatus.js
+++ config/baseResponseStatus.js
@@ -41,6 +41,8 @@ module.exports = {
     SIGNIN_INACTIVE_ACCOUNT : { "isSuccess": false, "code": 3005, "message": "비활성화 된 계정입니다. 고객센터에 문의해주세요." },
     SIGNIN_WITHDRAWAL_ACCOUNT : { "isSuccess": false, "code": 3006, "message": "탈퇴 된 계정입니다. 고객센터에 문의해주세요." },
 
+    //sksksksk
+
     //Connection, Transaction 등의 서버 오류
     DB_ERROR : { "isSuccess": false, "code": 4000, "message": "데이터 베이스 에러"},
     SERVER_ERROR : { "isSuccess": false, "code": 4001, "message": "서버 에러"},
